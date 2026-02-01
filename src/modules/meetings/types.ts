1d07c30fb52937675d92649ffa23fba6895dc5cd
HarshaVardhanRobba <20211a05p3@bvrit.ac.in>
2026-02-01 02:06:22 -0600

    24 - Background Jobs - Done

--- src/modules/meetings/types.ts
+++ src/modules/meetings/types.ts
@@ -10,4 +10,12 @@ export enum MeetingStatus {
     completed  = "completed",
     upcomming = "upcomming",
     cancelled = "cancelled"
+};
+
+export type StreamTranscriptItem = {
+    speaker_id: string;
+    text: string;
+    type : string;
+    start_ts: number;
+    stop_ts: number;
 }
